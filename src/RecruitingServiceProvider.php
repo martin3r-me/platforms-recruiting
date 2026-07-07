c703949a5599adec87b01e3efe4afec418ad0e27
SEBASTIAN HAUSTEIN <s.haustein@broichcatering.com>
2026-07-07 16:11:45 +0200

    feat(zas): Command recruiting:zas-crm-contact-backfill (dry-run/limit/imported-only)

--- src/RecruitingServiceProvider.php
+++ src/RecruitingServiceProvider.php
@@ -48,6 +48,7 @@ class RecruitingServiceProvider extends ServiceProvider
                 \Platform\Recruiting\Console\Commands\BackfillImageVariants::class,
                 \Platform\Recruiting\Console\Commands\DeleteEmployee::class,
                 \Platform\Recruiting\Console\Commands\ZasReExportByBookingDate::class,
+                \Platform\Recruiting\Console\Commands\ZasCrmContactBackfill::class,
                 \Platform\Recruiting\Console\Commands\FlynkReconcile::class,
             ]);
         }
